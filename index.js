e48313f04c1d917b3fba7b3a9aa85d8d9890c75c
codingisreallyhard <florinconstantin99@yahoo.com>
2022-10-25 14:22:27 +0300

    initial commit

--- index.js
+++ index.js
@@ -12,14 +12,6 @@ const db = mysql.createPool({
 app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
-
 app.post("/create", (req, res) => {
   const name = req.body.name;
   const sku = req.body.sku;
